f4d72e4cd8f1f0135a3cd3d2b2e15cd0b1d62685
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2022-10-20 11:14:54 +0800

    used promise.all to fetch data concurrently

--- stock-tracker/src/components/StockList.jsx
+++ stock-tracker/src/components/StockList.jsx
@@ -2,22 +2,52 @@ import finnHub from "../api/finnHub";
 import { useState, useEffect} from "react";
 
 export const StockList = ()=>{
+    const [stock,setStock] = useState();
     const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"]);
     useEffect(()=>{
+        let isMounted = true;
         const fetchData = async()=>{
+            const responses = [];
             try{
-                const response = await finnHub.get("/quote",{
+                const responses = Promise.all(
+                    [
+                    finnHub.get("/quote",{
+                    params:{
+                        symbol:"GOOGL"
+                    }
+                }),
+                 finnHub.get("/quote",{
                     params:{
                         symbol:"MSFT"
                     }
-                });
-                console.log(response)
+                }),
+                finnHub.get("/quote",{
+                    params:{
+                        symbol:"AMZN"
+                    }
+                })
+            ]
+        //     watchList.map((x)=>{
+        //         finnHub.get("/quote",{
+        //                     params:{
+        //                         symbol:x
+        //                     }
+        //     })
+        // }
+        //         ),)
+                )
+
+                console.log(responses);
+                if(isMounted){
+                    setStock(responses)
+                }
             }
             catch(err){
 
             }
         }
         fetchData()
+        return()=> (isMounted=false)
     }, [])
     
     return (
