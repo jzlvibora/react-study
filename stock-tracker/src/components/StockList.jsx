e10e39225ddbe8625ffd26866e1ce664c9bf19f6
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2022-10-20 20:32:27 +0800

    populate table with data

--- stock-tracker/src/components/StockList.jsx
+++ stock-tracker/src/components/StockList.jsx
@@ -2,55 +2,79 @@ import finnHub from "../api/finnHub";
 import { useState, useEffect} from "react";
 
 export const StockList = ()=>{
-    const [stock,setStock] = useState();
+    const [stock,setStock] = useState([]);
     const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"]);
     useEffect(()=>{
         let isMounted = true;
         const fetchData = async()=>{
-            const responses = [];
+           
             try{
-                const responses = Promise.all(
-                    [
-                    finnHub.get("/quote",{
-                    params:{
-                        symbol:"GOOGL"
+                const responses = await Promise.all(
+                    watchList.map((x)=>{
+                        return finnHub.get("/quote",{
+                            params:{
+                                symbol:x
+                            }
+                        })
                     }
-                }),
-                 finnHub.get("/quote",{
-                    params:{
-                        symbol:"MSFT"
-                    }
-                }),
-                finnHub.get("/quote",{
-                    params:{
-                        symbol:"AMZN"
-                    }
-                })
-            ]
-        //     watchList.map((x)=>{
-        //         finnHub.get("/quote",{
-        //                     params:{
-        //                         symbol:x
-        //                     }
-        //     })
-        // }
-        //         ),)
-                )
-
+                    )
+                    )
                 console.log(responses);
+                const data = responses.map((res)=>{
+                    return{
+                        data:res.data,
+                        symbol:res.config.params.symbol
+                    }
+                  
+                })
+
+                console.log(data);
+                console.log('Stock',stock)
+
                 if(isMounted){
-                    setStock(responses)
+                    setStock(data)
                 }
             }
+
             catch(err){
 
             }
         }
+
         fetchData()
         return()=> (isMounted=false)
     }, [])
     
     return (
-        <div>StockList</div>
+        <table className="table hover mt-5">
+            <thead style={{color:"rgb(78,89,102"}}>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Chg</th>
+                    <th scope="col">Chg%</th>
+                    <th scope="col">High</th>
+                    <th scope="col">Low</th>
+                    <th scope="col">Open</th>
+                    <th scope="col">PClose</th>
+                </tr>
+            </thead>
+            <tbody>
+                {stock.map((stockData)=>{
+                    return (
+                        <tr className="table-row" key={stockData.symbol}>
+                            <th scope="row">{stockData.symbol}</th>
+                            <td>{stockData.data.c}</td>
+                            <td>{stockData.data.d}</td>
+                            <td>{stockData.data.dp}</td>
+                            <td>{stockData.data.h}</td>
+                            <td>{stockData.data.l}</td>
+                            <td>{stockData.data.o}</td>
+                            <td>{stockData.data.pc}</td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
     )
 }
